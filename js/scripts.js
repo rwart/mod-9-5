1b1d765500a744ea02e5c21848709fa667d210ff
rwart <32432397+rwart@users.noreply.github.com>
2017-10-17 15:02:11 +0200

    ready for review

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
+(function () {
+  'use strict';
+  var rwButtons = document.getElementsByClassName('button');
 
+  for (var i = 0; i < rwButtons.length; i++) {
+    alert(rwButtons[i].innerText);
+  }
+  /*
+  for (var i = 0, button; button = rwButtons[i]; i++) {
+    alert(button.innerText);
+  }
+  */
+})();
